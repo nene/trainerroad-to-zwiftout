fb6c83aa6f0ddbf4202b7016f54f67cca2da4aae
Rene Saarsoo <nene@triin.net>
2022-12-27 16:51:26 +0200

    Eliminate accidental use of var

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import trainerroadToZwiftout from "../trainerroad-to-zwiftout.js";
 
-var json = JSON.parse(fs.readFileSync(0, "utf-8"));
+const json = JSON.parse(fs.readFileSync(0, "utf-8"));
 
 console.log(trainerroadToZwiftout(json.Workout));
